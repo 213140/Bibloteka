3ced9cb15ae8cd4640390f5efb19dcba3a190c6b
ANDZEJ MACKEVIC <ANDZEJ_MACKEVIC@jabil.com>
2020-04-05 13:52:56 +0200

    Modified new book adding function

--- Bibloteka/MainWindow.xaml.cs
+++ Bibloteka/MainWindow.xaml.cs
@@ -23,6 +23,7 @@ namespace Bibloteka
     public partial class MainWindow : Window
     {
         MySqlConnection connection = new MySqlConnection("datasource=localhost;port=3306;username=admin;password=2610Andre");
+        String sqlSelectQuery;
         public MainWindow()
         {
             InitializeComponent();
@@ -30,6 +31,13 @@ namespace Bibloteka
         //List<Book> Book_list = new List<Book>();
         //Book_list.Add(new Book(text_box_book_name.Text, "A.M.", 123, false));
         //text_box_result.Text = Book_list[0].book_name + " " + Book_list[0].book_author;
+
+        public MySqlDataReader Create_MySqlDataReader(MySqlConnection con, string SQL_code)
+        {
+            sqlSelectQuery = SQL_code;
+            MySqlCommand cmd = new MySqlCommand(sqlSelectQuery, con);
+            return cmd.ExecuteReader();
+        }
 
         private void Connect_button_Click(object sender, RoutedEventArgs e)
         {
@@ -53,6 +61,9 @@ namespace Bibloteka
 
         private void Button_getData_from_DB_Click(object sender, RoutedEventArgs e)
         {
+            sqlSelectQuery = "SELECT * FROM bibloteka.books WHERE bibloteka.books.book_name = '" + textbox_newbookNAME.Text + "' AND bibloteka.books.book_author = '" + textbox_newbookAUTHOR.Text + "'";
+            MySqlDataReader dr = Create_MySqlDataReader(connection, sqlSelectQuery);
+
             int new_book_quantity = 0;
             int if_available = 0;
             try
@@ -62,17 +73,30 @@ namespace Bibloteka
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message);
+                return;
             }
             if (new_book_quantity > 0) { if_available = 1;}
-            String sqlSelectQuery = "INSERT INTO bibloteka.books VALUES ('" + textbox_newbookID.Text + "', '" + textbox_newbookNAME.Text + "', '" + textbox_newbookAUTHOR.Text + "', '" + textbox_newbookPAGES.Text + "', '" + textbox_newbookQUANTITY.Text + "', '" + if_available.ToString() + "')";
-            MessageBox.Show(sqlSelectQuery);
-            MySqlCommand cmd = new MySqlCommand(sqlSelectQuery, connection);
-            MySqlDataReader dr = cmd.ExecuteReader();
-            dr.Read();
-            //if(dr.Read())
-           // {
-             //   textbox_data_from_DB.Text = dr[0].ToString() + " " + dr[1].ToString(); ;
-          //  }
+
+            if (dr.Read())
+            {
+                int present_quantity = dr.GetInt32(dr.GetOrdinal("book_quantity"));
+                int current_ID = dr.GetInt32(dr.GetOrdinal("ID"));
+                dr.Close();
+
+                sqlSelectQuery = "UPDATE bibloteka.books SET bibloteka.books.book_quantity = '" + (present_quantity + new_book_quantity).ToString() + "' WHERE bibloteka.books.ID = '" + current_ID.ToString() + "'";
+                dr = Create_MySqlDataReader(connection, sqlSelectQuery);
+
+                dr.Close();
+                return;
+            }
+            else
+            {
+                dr.Close();
+                sqlSelectQuery = "INSERT INTO bibloteka.books VALUES ('" + textbox_newbookID.Text + "','" + textbox_newbookNAME.Text + "','" + textbox_newbookAUTHOR.Text + "','" + textbox_newbookPAGES.Text + "','" + textbox_newbookQUANTITY.Text + "','" + if_available.ToString() + "')";
+                dr = Create_MySqlDataReader(connection, sqlSelectQuery);
+                dr.Close();
+                return;
+            }
             
         }
     }
